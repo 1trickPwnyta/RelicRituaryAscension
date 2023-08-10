24b69a80c357e6a6c8b36d76ca2802b7ea49768d
Sarah Pace <sarahpace37@gmail.com>
2023-08-09 19:48:28 -0700

    initial commit

---
+++ Source/RelicRituaryAscensionMod.cs
@@ -0,0 +1,15 @@
+﻿using Verse;
+
+namespace RelicRituaryAscension
+{
+    public class RelicRituaryAscensionMod : Mod
+    {
+        public const string PACKAGE_ID = "relicrituaryascension.1trickPwnyta";
+        public const string PACKAGE_NAME = "Relic Rituary Ascension";
+
+        public RelicRituaryAscensionMod(ModContentPack content) : base(content)
+        {
+            Log.Message($"[{PACKAGE_NAME}] Loaded.");
+        }
+    }
+}
---
+++ Source/RitualOutcomeEffectWorker_RelicAscension.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Timers;
+using UnityEngine;
+using Verse;
+using Verse.Sound;
+
+namespace RimWorld
+{
+    public class RitualOutcomeEffectWorker_RelicAscension : RitualOutcomeEffectWorker
+    {
+        public override bool SupportsAttachableOutcomeEffect
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public RitualOutcomeEffectWorker_RelicAscension()
+        {
+
+        }
+
+        public RitualOutcomeEffectWorker_RelicAscension(RitualOutcomeEffectDef def) : base(def)
+        {
+
+        }
+
+        public override void Apply(float progress, Dictionary<Pawn, int> totalPresence, LordJob_Ritual jobRitual)
+        {
+            SoundDefOf.Archotech_Invoked.PlayOneShotOnCamera();
+            ScreenFader.StartFade(Color.white, 5f);
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(delegate(object source, ElapsedEventArgs e)
+            {
+                timer.Stop();
+                StringBuilder stringBuilder = new StringBuilder();
+                List<Pawn> list = totalPresence.Keys.ToList();
+                foreach (Pawn pawn in list)
+                {
+                    stringBuilder.AppendLine("   " + pawn.LabelCap);
+                    Find.StoryWatcher.statsRecord.colonistsLaunched++;
+                }
+                GameVictoryUtility.ShowCredits(GameVictoryUtility.MakeEndCredits("RelicRituaryAscension_GameOverRelicRitualInvokedIntro".Translate(), "RelicRituaryAscension_GameOverRelicRitualInvokedEnding".Translate(), stringBuilder.ToString(), "RelicRituaryAscension_GameOverColonistsAscended", list), SongDefOf.ArchonexusVictorySong);
+            });
+            timer.Interval = 5000;
+            timer.Start();
+        }
+
+        public override OutcomeChance GetForcedOutcome(Precept_Ritual ritual, TargetInfo ritualTarget, RitualObligation obligation, RitualRoleAssignments assignments)
+        {
+            return this.def.outcomeChances[0];
+        }
+
+        public override IEnumerable<string> BlockingIssues(Precept_Ritual ritual, TargetInfo target, RitualRoleAssignments assignments)
+        {
+            yield break;
+        }
+    }
+}

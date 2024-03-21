b5ea45a6af59dc64f3e1c6d48888ddd59002d891
1trickPwnyta <sarahpace37@gmail.com>
2024-03-21 16:35:15 -0700

    Update to 1.5

---
+++ 1.5/Source/Debug.cs
@@ -0,0 +1,12 @@
+﻿namespace RelicRituaryAscension
+{
+    public static class Debug
+    {
+        public static void Log(string message)
+        {
+#if DEBUG
+            Verse.Log.Message($"[{RelicRituaryAscensionMod.PACKAGE_NAME}] {message}");
+#endif
+        }
+    }
+}
---
+++ 1.5/Source/RitualOutcomeEffectWorker_RelicAscension.cs
@@ -0,0 +1,52 @@
+﻿using RelicRituaryAscension;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Timers;
+using UnityEngine;
+using Verse;
+using Verse.Sound;
+
+namespace RimWorld
+{
+    public class RitualOutcomeEffectWorker_RelicAscension : RitualOutcomeEffectWorker
+    {
+        public override bool SupportsAttachableOutcomeEffect
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public RitualOutcomeEffectWorker_RelicAscension()
+        {
+
+        }
+
+        public RitualOutcomeEffectWorker_RelicAscension(RitualOutcomeEffectDef def) : base(def)
+        {
+
+        }
+
+        public override void Apply(float progress, Dictionary<Pawn, int> totalPresence, LordJob_Ritual jobRitual)
+        {
+            SoundDefOf.Archotech_Invoked.PlayOneShotOnCamera();
+            ScreenFader.StartFade(Color.white, 5f);
+            EndGameTimer.timeLeft = 5f;
+            GameObject.Find("GameRoot").AddComponent<EndGameTimer>();
+            RelicAscensionCountdown.pawns = totalPresence.Keys.ToList();
+        }
+
+        public override RitualOutcomePossibility GetForcedOutcome(Precept_Ritual ritual, TargetInfo ritualTarget, RitualObligation obligation, RitualRoleAssignments assignments)
+        {
+            return this.def.outcomeChances[0];
+        }
+
+        public override IEnumerable<string> BlockingIssues(Precept_Ritual ritual, TargetInfo target, RitualRoleAssignments assignments)
+        {
+            yield break;
+        }
+    }
+}
